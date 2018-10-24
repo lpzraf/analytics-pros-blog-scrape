0867ce1fcbc1ae6a829d109bdb7956570ed4550d
Rafael Lopez <lpz.raf@gmail.com>
2018-10-24 23:56:12 +0000

    first commit

---
+++ lib/analytics/pros/blog/scrape.rb
@@ -0,0 +1,11 @@
+require "analytics/pros/blog/scrape/version"
+
+module Analytics
+  module Pros
+    module Blog
+      module Scrape
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/analytics/pros/blog/scrape/version.rb
@@ -0,0 +1,9 @@
+module Analytics
+  module Pros
+    module Blog
+      module Scrape
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
