870585431e975e8ad90d596893cc10be9199e727
Rafael Lopez <lpz.raf@gmail.com>
2018-10-27 17:53:32 +0000

    first part of the skeleton done

--- lib/blog_scrape/cli.rb
+++ lib/blog_scrape/cli.rb
@@ -17,15 +17,19 @@ class BlogScrape::CLI
   end
   
   def menu
-    puts "What would you like to learn today? Enter a category number to get a list of articles, or type exit to enter:"
     input = nil
     while input != "exit"
+      puts "What would you like to learn today? Enter a category number to get a list of articles, type list to get the categories list again or type exit to finish the program:"
       input = gets.strip.downcase
       case input
       when "1"
         puts "More info on category 1..."
       when "2"
         puts "More info on category 2..."
+      when "list"
+        list_categories
+      else
+        puts "Oops! What you typed does not exist, why don't you try a new number?"
       end
   end
 end
