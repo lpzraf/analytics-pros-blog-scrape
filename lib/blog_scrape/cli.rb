61820471d229e29ba0e0c492d0e3a0067e7d7144
Rafael Lopez <lpz.raf@gmail.com>
2018-10-27 17:34:25 +0000

    testing the code of my first section

--- lib/blog_scrape/cli.rb
+++ lib/blog_scrape/cli.rb
@@ -2,7 +2,35 @@
 
 class BlogScrape::CLI
   def call 
-    puts "Welcome to the Analytics Pros blog vault!"
+    list_categories
+    menu
+    goodbye
   end
   
+  def list_categories
+    puts "Welcome to the Analytics Pros blog vault!"
+    puts <<-DOC.gsub /^\s*/,''
+      1. Advanced Filters
+      2. AdWords
+      3. Augmented Analytics
+    DOC
+  end
+  
+  def menu
+    puts "What would you like to learn today? Enter a category number to get a list of articles, or type exit to enter:"
+    input = nil
+    while input != "exit"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "More info on category 1..."
+      when "2"
+        puts "More info on category 2..."
+      end
+  end
+end
+
+  def goodbye
+    "See you next time!"
+  end
 end
