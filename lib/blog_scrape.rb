f633672f63bb015dae74833ca139f89acf3c7a9b
Rafael Lopez <lpz.raf@gmail.com>
2018-10-30 02:35:00 +0000

    added Nokogiri

--- lib/blog_scrape.rb
+++ lib/blog_scrape.rb
@@ -1,3 +1,5 @@
+require "nokogiri"
+require "pry"
 require_relative "./blog_scrape/version"
 require_relative './blog_scrape/cli'
 require_relative './blog_scrape/category'
--- lib/blog_scrape/category.rb
+++ lib/blog_scrape/category.rb
@@ -1,26 +1,28 @@
 class BlogScrape::Category
   attr_accessor :name, :url
   def self.today
-    #should return a bunch of Category
+    #scrape AP's blog and return the categories
+    self.scrape_categories
+  end
    
-    # puts <<-DOC.gsub /^\s*/,''
-    #   1. Advanced Filters
-    #   2. AdWords
-    #   3. Augmented Analytics
-    # DOC
-   
-    category_1 = self.new
-    category_1.name = "Advanced Filters"
-    category_1.url = "https://www.analyticspros.com/blog/category/advanced-filters/"
+   def self.scrape_categories
+    #go to AP, find categories, extract properties, instantiate categories
+     
+     categories = []
+     
+    # category_1 = self.new
+    # category_1.name = "Advanced Filters"
+    # category_1.url = "https://www.analyticspros.com/blog/category/advanced-filters/"
   
-    category_2 = self.new
-    category_2.name = "AdWords"
-    category_2.url = "https://www.analyticspros.com/blog/category/adwords/"
+    # category_2 = self.new
+    # category_2.name = "AdWords"
+    # category_2.url = "https://www.analyticspros.com/blog/category/adwords/"
     
-    category_3 = self.new
-    category_3.name = "Augmented Analytics"
-    category_3.url = "https://www.analyticspros.com/blog/category/augmented-analytics/"
+    # category_3 = self.new
+    # category_3.name = "Augmented Analytics"
+    # category_3.url = "https://www.analyticspros.com/blog/category/augmented-analytics/"
   
-    [category_1,category_2,category_3]
+    # [category_1,category_2,category_3]
+    categories
   end
 end
