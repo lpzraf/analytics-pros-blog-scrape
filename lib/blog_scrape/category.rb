47caa2f491b21fd3aa194ce22a855bade1011aa6
Rafael Lopez <lpz.raf@gmail.com>
2018-11-04 00:14:49 +0000

    commmented the .today  method

--- lib/blog_scrape/category.rb
+++ lib/blog_scrape/category.rb
@@ -1,17 +1,18 @@
 class BlogScrape::Category
   attr_accessor :name, :category_url, :blog_post_name, :blog_post_url, :blog_post_description  
-  def self.today
-    #scrape AP's blog and return the categories
-    self.scrape_categories
-  end
+  
+  
+  # def self.today
+  #   #scrape AP's blog and return the categories
+  #   self.scrape_categories
+  # end
    
-   
-  def self.scrape_categories
-    #go to AP, find categories, extract properties, instantiate categories
-     categories = []
-     categories << self.scrape_ap_blog_categories
-     categories
-  end
+  # def self.scrape_categories
+  #   #go to AP, find categories, extract properties, instantiate categories
+  #   categories = []
+  #   categories << self.scrape_ap_blog_categories
+  #   categories
+  # end
   
   
   def self.scrape_ap_blog_categories
