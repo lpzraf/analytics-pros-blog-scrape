ee588a838d1550693394369a33ee9c6c1c9db1a6
Rafael Lopez <lpz.raf@gmail.com>
2018-11-06 01:22:37 +0000

    started blog post list scraper

--- lib/blog_scrape/scrape.rb
+++ lib/blog_scrape/scrape.rb
@@ -7,5 +7,16 @@ class BlogScrape::Scrape
   def self.scrape_categories
     self.get_page.css("li.cat-item")
   end
+  
+  def self.scrape_blog_post(url)
+    doc = Nokogiri::HTML(open(url))
+
+    post_name = doc.css("h2.entry-title").text
+    post_url = doc.css("h2.entry-title a").attribute("href").text
+    post_description = doc.css("h2.entry-title a").attribute("href").text
+
+    blog_post_list = {:post_name => post_name, :post_url => post_url, :post_description => post_description}
+    blog_post_list
+  end
 
 end
