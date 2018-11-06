93bb0123bda8926e495ad4305ada84d23c4d98cf
Rafael Lopez <lpz.raf@gmail.com>
2018-11-06 03:13:11 +0000

    eliminated unnesessary methods from .category

--- lib/blog_scrape/category.rb
+++ lib/blog_scrape/category.rb
@@ -28,34 +28,4 @@ class BlogScrape::Category
      category.save
     end
   end
-  
-  
-  def self.scrape_title
-    #how to dynamically change the url whre the blog post are?
-    doc = Nokogiri::HTML(open("https://www.analyticspros.com/blog/category/advanced-filters/"))
-    
-    doc.search("h2.entry-title").each do |p_title|
-      post_title = self.new
-      post_title.post_name = p_title.text
-    end
-  end
-  
-  # def self.scrape_category_url
-  #   doc = Nokogiri::HTML(open("https://www.analyticspros.com/blog/mobile/react-native-firebase-gtm-integration-installation/"))
-    
-  #   doc.search("li.cat-item a").attribute("href").each do |categ_url|
-  #     cat_url = self.new
-  #     cat_url.category_url = categ_url.text
-  #     binding.pry
-  #   end
-  # end
-  
-  
-  # def blog_post_url
-  #   doc = Nokogiri::HTML(open("https://www.analyticspros.com/blog/category/advanced-filters/"))
-  #   @blog_post_url = doc.search("h2.entry-title a").attribute("href").text
-  #   binding.pry
-  #   end
-  
-  
 end
